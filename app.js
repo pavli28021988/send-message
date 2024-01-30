3bd3274a64779fcd40159d4dc22e579d7ebe429d
Pavlina Ivanova <pavlina33331988@gmail.com>
2024-01-30 17:16:59 +0200

    added
    added to git

---
+++ app.js
@@ -0,0 +1,31 @@
+﻿const form =  document.getElementById('message-form');
+const message = document.getElementById('message');
+const feedback = document.querySelector('.feedback');
+const messageContent = document.querySelector('.message-content');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(message.value === ''){
+        /* feedback.style.display = 'block'; */
+
+        feedback.classList.add('show');
+        setTimeout(()=>{
+
+            feedback.classList.remove('show');
+
+        },4000)
+    }else{
+
+        messageContent.textContent = message.value;
+        message.value = '';
+
+    }
+
+
+
+})
+
+/* form.addEventListener('submit', (e)=>{
+
+})
+ */
